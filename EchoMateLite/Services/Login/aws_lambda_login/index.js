902d6792faab012ebe844dac7fc41da7093abdcb
Amit Narayan <amitnarayan707@gmail.com>
2025-04-26 10:26:38 +0530

    add login function

--- EchoMateLite/Services/Login/aws_lambda_login/index.js
+++ EchoMateLite/Services/Login/aws_lambda_login/index.js
@@ -1,30 +1,32 @@
 const AWS = require('aws-sdk');
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
+const { saveCognitoToken } = require('./session');
+
 exports.handler = async (event) => {
     const userPoolId = process.env.USER_POOL_ID;
     const clientId = process.env.CLIENT_ID;
-    const username = event.username;
-    const password = event.password;
+    const email = JSON.parse(event.body).email;
+    const password = JSON.parse(event.body).password;
     
     const params = {
         AuthFlow: 'USER_PASSWORD_AUTH',
         ClientId: clientId,
         AuthParameters: {
-            USERNAME: username,
+            email: email,
             PASSWORD: password
         }
     };
     
     try {
         const response = await cognito.initiateAuth(params).promise();
+        saveCognitoToken(email, response.AuthenticationResult )
         return {
             statusCode: 200,
-            body: JSON.stringify({
-                id_token: response.AuthenticationResult.IdToken,
-                access_token: response.AuthenticationResult.AccessToken,
-                refresh_token: response.AuthenticationResult.RefreshToken
-            })
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(response)
         };
     } catch (error) {
         return {
