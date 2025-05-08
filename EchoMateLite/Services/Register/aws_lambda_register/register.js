7fb0e7b3c95f24a7c7ad465490b5fea549a5a20a
Amit Narayan <amitnarayan707@gmail.com>
2025-05-08 23:06:37 +0530

    fix

--- EchoMateLite/Services/Register/aws_lambda_register/register.js
+++ EchoMateLite/Services/Register/aws_lambda_register/register.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const corsHeaders = require('./corsHeaders');
+const { v4: uuidv4 } = require('uuid');
 
 AWS.config.update({ region: process.env.MY_AWS_REGION });
 const cognito = new AWS.CognitoIdentityServiceProvider();
@@ -6,23 +8,22 @@ const cognito = new AWS.CognitoIdentityServiceProvider();
 export default Register = async (event) => {
     const userPoolId = process.env.USER_POOL_ID;
     const clientId = process.env.CLIENT_ID;
-    const email = JSON.parse(event.body).email;
-    const password = JSON.parse(event.body).password;
-
     const register = JSON.parse(event.body).userRegister;
     const userDetails = JSON.parse(event.body).userDetails;
 
+    const userId = ~uuidv4();
+    userDetails.userId = userId;
 
 
     const params = {
         ClientId: clientId,
         UserPoolId: userPoolId,
-        Username: email,
-        Password: password,
+        Username: userId,
+        Password: register.password,
         UserAttributes: [
             {
                 Name: 'email',
-                Value: email
+                Value: register.email
             }
         ]
     };
@@ -42,34 +43,19 @@ export default Register = async (event) => {
         if (response.UserConfirmed) {
             return {
                 statusCode: 200,
-                headers: {
-                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'POST, OPTIONS'
-                },
+                headers: corsHeaders,
                 body: JSON.stringify(resData)
             };
         }
         return {
             statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': 'POST, OPTIONS'
-            },
+            headers: corsHeaders,
             body: JSON.stringify(resData)
         };
     } catch (error) {
         return {
             statusCode: 400,
-            headers: {
-                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': 'POST, OPTIONS'
-            },
+            headers: corsHeaders,
             body: JSON.stringify(error.message)
         };
     }
